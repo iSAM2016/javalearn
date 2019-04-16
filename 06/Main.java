f360af16e21a42f8603fb489f924466cfb40ba1f
isam2016 <suhailong@shouzan365.com>
2019-04-17 07:20:56 +0800

    save

--- 06/Main.java
+++ 06/Main.java
@@ -5,6 +5,7 @@
  */
 
 class Car {
+
     // 成员变量存在内存中，因为对象的存在才在内存中存在
     protected String color = "red";
     private int age = 10;
@@ -12,6 +13,28 @@ class Car {
 
     void run() {
         System.out.println("age" + this.age);
+    }
+
+}
+
+class Persion {
+    {
+        // 构造代码块
+        System.out.println("cry");
+    }
+
+    // 构造函数
+    // 当一个函数中没有构造函数的时候，系统会默认自动添加空参数的构造函数
+    Persion() {
+        System.out.println("A persion");
+    }
+
+    Persion(String name) {
+        System.out.println("B" + name + "persion");
+    }
+
+    Persion(String name, int age) {
+        System.out.println("C" + name + "age" + age);
     }
 
 }
@@ -35,5 +58,8 @@ public class Main {
         c.num = 20;
         System.out.println("color" + c.num);
         // c.run();
+
+        Persion p1 = new Persion("name", 20);
+
     }
 }
--- 07/Main.java
+++ 07/Main.java
@@ -33,12 +33,34 @@
  * 
  * 4. 抽象类中的方法要被使用，必须有子类复写所有的抽象方法后，建立子类对象调用，如果子类覆盖了部分抽象方法，那么该子类 还是一个抽象类
  * 
- * 
- * 抽象类和一般类不同：
+ * e 抽象类和一般类不同：
  * 
  * 正常描述事务，只不过是该事务出现了一些看不懂的东西。
  * 
  * 抽象只能描述方法，可以不定义抽象方法
+ * 
+ * 
+ * 
+ * 模板方法：
+ * 
+ * 在定义功能的时候，功能的一部分是确定的， 但是有一部分的是不确定的，而确定的部分固定，不确定的部分暴露出去
+ * 
+ * 多态的体现是体现在类上
+ * 
+ * 
+ * 1. 多态的体现 父类的引用指向了自己的子类对象，父类的引用也可以接收自己的子类对象
+ * 
+ * 2. 多态的前提 必须是类和类的关系，要么继承要么实现，通常还有个前提，存在覆盖
+ * 
+ * 3. 多态的应用
+ * 
+ * 4. 多态的好处 提高了程序的扩展性，
+ * 
+ * 5. 弊端 但是只能使用父类的引用访问父类中的成员
+ * 
+ * 
+ * 多态的好处是：多态简化了对象调用
+ * 
  * 
  */
 
@@ -99,13 +121,161 @@ final class Final {
     void show() {
         System.out.println("final");
     }
+
+}
+
+abstract class Animal {
+
+    abstract void eat();
+
+}
+
+class Cat extends Animal {
+    public void eat() {
+        System.out.println("吃鱼");
+    };
+
+    public void catchMouse() {
+        System.out.println("抓老鼠");
+    };
+
+}
+
+class Dog extends Animal {
+    public void eat() {
+        System.out.println("骨头");
+    };
+
+    public void protectHpuse() {
+        System.out.println("看家");
+    };
+
 }
 
 public class Main {
     public static void main(String[] args) {
-        Z1 z = new Z1(0);
-        z.show();
-        System.out.println(z.houname);
 
+        Cat c = new Cat();
+        Dog d = new Dog();
+        // function(c);
+        // function(d);
+
+        Animal a = new Cat(); // 类型提升 向上转型
+        a.eat();
+        // 如果想调用猫的独特方法 如何操作
+        // 强制将父类型的引用，转成子类型，向下转型
+        Cat cat = (Cat) a;
+
+        cat.catchMouse();
+
+        Do ds = new Do();
+        ds.dosome(new BaseStudent());
+        ds.dosome(new AdvStudent());
+
+        /**
+         * 不要出现这样的情况，将父类对象转成子类类型
+         * 
+         * 我们能转换的是父类应用指向了自己的子类对象时，该应用可以提升，也可以被强制转换
+         * 
+         * 多态始终变化的是自己
+         * 
+         * // Animal a = new Aaimal() cat c = (cat) A
+         */
+
+        // function(c);
+        // function(d);
+
+        // Z1 z = new Z1(0);
+        // z.show();
+        // System.out.println(z.houname);
+
+        // SubTime time = new SubTime();
+        // time.getTime();
+
+    }
+
+    public static void function(Animal animal) {
+        animal.eat();
+    }
+
+    // public static void function(Cat c) {
+    // c.eat();
+    // }
+
+    // public static void function(Dog c) {
+    // c.eat();
+    // }
+}
+
+/** 练习 */
+abstract class Employee {
+    private String name;
+    private String id;
+    private double pay;
+
+    Employee(String name, String id, double pay) {
+        this.name = name;
+        this.id = id;
+        this.pay = pay;
+    }
+
+    public abstract void work();
+}
+
+class Mangeer extends Employee {
+    private int bonus;
+
+    Mangeer(String name, String id, double pay, int bonus) {
+        super(name, id, pay);
+        this.bonus = bonus;
+    }
+
+    public void work() {
+        System.out.println("mage");
+    };
+}
+
+// 模板方法
+abstract class GetTime {
+    public final void getTime() {
+        long start = System.currentTimeMillis();
+        runcode();
+        long end = System.currentTimeMillis();
+        System.out.println("all-time:" + (end - start));
+
+    }
+
+    public abstract void runcode();
+}
+
+class SubTime extends GetTime {
+    public void runcode() {
+        for (int i = 0; i < 1000; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+/******* */
+class BaseStudent extends Student {
+    public void Study() {
+        System.out.println("base");
+    }
+
+    public void Sleep() {
+        System.out.println("睡觉");
+    }
+}
+
+class AdvStudent extends Student {
+    public void Study() {
+        System.out.println("adv study");
+    }
+}
+
+class Do {
+    public static void dosome(Student bs) {
+        bs.Study();
+        bs.sleep();
     }
 }
