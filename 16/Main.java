48c72ca34cab1fd1ead33f494de7946c68e24377
isam2016 <suhailong@shouzan365.com>
2019-05-15 15:36:40 +0800

    类型转换

---
+++ 16/Main.java
@@ -0,0 +1,46 @@
+/*
+ * 
+ * 将基本类型封装成对象的好处在于对象中定义更多方法操作改数据
+ * 
+ * 常用于基本数据类型 与字符串之间的转换（8种基本类型）
+ * 
+ * byte Byte
+ * 
+ * short Short
+ * 
+ * int Integer
+ * 
+ * long Long
+ * 
+ * float Float
+ * 
+ * double Double
+ * 
+ * char Character
+ * 
+ * boolean Boolean
+ * 
+ * 
+ * 基本类型转化为字符串
+ * 
+ * 1. 基本类型数值 + ‘’
+ * 
+ * 2. 使用string类型中的stringOf(基本数据类型)
+ *
+ * 字符串- 基本数据类型
+ * 
+ * 使用包装类型的静态方法 xxx parsexxx(xxx 类型的字符串)
+ * 
+ * int parseInt()
+ * 
+ * long parseLong()
+ * 
+ * boolean parseBoolean()
+ * 
+ * char 没有方法
+ */
+class a {
+    public static void main(String[] args) {
+
+    }
+}
